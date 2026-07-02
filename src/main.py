cddd8907298645c1cf30c5e3f39c743626dc965e
xmh3698 <xh2550@columbia.edu>
2026-07-02 16:13:50 -0400

    Add 1D case training NO

--- src/main.py
+++ src/main.py
@@ -1,74 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import utils
 from torch.nn import BCELoss
 import deepxde as dde
 import torch
-import time
-import math
-import numpy as np
-import torch
-import cma
-import matplotlib.pyplot as plt
-import deepxde as dde
-from src import utils
-import subsample_data
-from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
-import argparse
 
 """
 In this script, we will train a DeepONet that takes PCA transformed coordinates
 as input and outputs the cooresponding pattern. We will only use first 25 co-
-ordinates. 
+ordinates. A subset with efficiency >= 0.9, wavelength = 850nm, and angle = 65 degrees.
 
 DeepONet takes function as input and output another function. The input function is
 1D-to-1D, same for the output function.
 """
-
-
-# Load dataset
-# ---------------------------
-# Parse command line arguments
-# ---------------------------
-parser = argparse.ArgumentParser(description="Run CMA-ES optimization with specified angle and wavelength.")
-parser.add_argument("--angle", type=int, required=True, help="Incident angle in degrees")
-parser.add_argument("--wavelength", type=int, required=True, help="Wavelength in nm")
-parser.add_argument("--seednum", type=int, default=None, help="Seed number for initial starting point")
-parser.add_argument("--dataid", type=int, required=True)
-args = parser.parse_args()
-
-angle = args.angle
-wavelength = args.wavelength
-
-# If seednum not provided, generate one randomly
-if args.seednum is None:
-    seednum = np.random.randint(0, 10**3)  # e.g., a random integer up to 1000
-    print(f"No seednum provided. Randomly generated seed: {seednum}")
-else:
-    seednum = args.seednum
-    print(f"Using provided seednum: {seednum}")
-
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 # Set random seed
-seed = 658 # The seed number for pre-trained neural operator
+seed = 658
 dde.config.set_random_seed(seed)
-utils.set_seed(seed)
 
 # Load dataset
-data_path = f"data/subsample/subsample_dataset_seed_{args.dataid}.npz"
-d = np.load(data_path)
-dataset_pattern = d['pattern']
-dataset_eff = d['efficiency']
-dataset_pattern = utils.preprocessing(dataset_eff)
+d = np.load('data/90_all_all_train.npz')
+X_train_branch = d['X1'][:,:25].astype(np.float32)
+X_train_trunk = d['X2'].astype(np.float32)
+y_train = d['y'].astype(np.float32)
+d = np.load('data/90_all_all_val.npz')
+X_test_branch = d['X1'][:,:25].astype(np.float32)
+X_test_trunk = d['X2'].astype(np.float32)
+y_test = d['y'].astype(np.float32)
+breakpoint()
 
-latents, _ = subsample_data.pca_transform_data(dataset_pattern,dataset_eff)
-y = (dataset_pattern + 1) / 2
-X_train_branch, X_test_branch, y_train, y_test = train_test_split(latents, y, train_size=0.8, random_state=42)
-X_train_trunk = np.array([[i] for i in range(len(y_train[0]))]) / float(len(y_train[0]))
-X_test_trunk = np.array([[i] for i in range(len(y_train[0]))]) / float(len(y_train[0]))
-X_test_trunk_torch = torch.tensor(X_test_trunk, dtype=torch.float32).to(device).requires_grad_(False)
 
 # Define the data
 data = dde.data.TripleCartesianProd(
@@ -157,11 +116,11 @@ class BetaScheduler(dde.callbacks.Callback):
 # Define the model
 model = dde.Model(data, net)
 # Compile and Train
-loss_fn = CustomLossPlus(alpha = 20)
+loss_fn = CustomLossPlus(alpha = 2)
 model.compile("adam", lr=0.001, loss = loss_fn, metrics=[binary_cross_entropy_np])
 for i in range(10):
     iterations = 40000
-    losshistory, train_state = model.train(iterations=iterations, model_save_path=f"model/model_ab{seed}.ckpt")
+    losshistory, train_state = model.train(iterations=iterations, model_save_path=f"model/model_{seed}.ckpt")
     model.net.scale_factor *= 2
 
 
@@ -169,8 +128,6 @@ for i in range(10):
 dde.utils.plot_loss_history(losshistory)
 plt.legend(['train loss', 'val loss', 'val metric'])
 plt.show()
-breakpoint()
 
 # Save loss history
-np.savez(f"model/model_ab{seed}_losshistory.npz", train_loss=losshistory.loss_train, val_loss=losshistory.loss_test, val_metric=losshistory.metrics_test)
-breakpoint()
+np.savez(f"model/model_{seed}_losshistory.npz", train_loss=losshistory.loss_train, val_loss=losshistory.loss_test, val_metric=losshistory.metrics_test)
